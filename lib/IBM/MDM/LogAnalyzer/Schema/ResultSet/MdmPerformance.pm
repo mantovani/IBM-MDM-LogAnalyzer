f9dfefd673325ae447592da6289d435408b53d8f
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2014-07-31 16:16:15 -0300

    response ms report add

--- lib/IBM/MDM/LogAnalyzer/Schema/ResultSet/MdmPerformance.pm
+++ lib/IBM/MDM/LogAnalyzer/Schema/ResultSet/MdmPerformance.pm
@@ -4,6 +4,10 @@ use strict;
 use warnings;
 use base 'DBIx::Class::ResultSet';
 
+use constant BLOCK => 10;
+
+use Data::Dumper;
+
 sub runs {
     my ( $self, $name, $operation ) = @_;
     [
@@ -111,4 +115,77 @@ sub json_transaction_avg {
           )->all
     ];
 }
+
+sub _min_max_transaction_run {
+    my ( $self, $name, $operation, $run ) = @_;
+    my @result = $self->search(
+        {
+            name      => $name,
+            operation => $operation,
+            run       => $run
+        },
+        {
+            select =>
+              [ { max => 'delay' }, { min => 'delay' }, { count => '*' } ],
+            as       => [ 'max', 'min', 'total' ],
+            group_by => [qw/name operation run/],
+        }
+    )->all;
+    return {
+        min   => $result[0]->get_column('min'),
+        max   => $result[0]->get_column('max'),
+        total => $result[0]->get_column('total'),
+    };
+}
+
+sub report_transaction {
+    my ( $self, $name, $operation ) = @_;
+    my $runs = $self->list_runs( $name, $operation );
+    my $stack = {};
+    foreach my $run ( @{$runs} ) {
+        my $values = $self->_min_max_transaction_run( $name, $operation, $run );
+        print Dumper $values;
+        my $diff  = $values->{max} - $values->{min};
+        my $total = $values->{total};
+        my $block = int $diff / BLOCK;
+        for my $unit ( 0 .. BLOCK ) {
+            push @{ $stack->{$run} }, map {
+                {
+                    count   => $_->get_column('total'),
+                    total   => $total,
+                    percent => $_->get_column('total') * 100 / $total,
+                    between => [
+                        ( $unit * $block ) ? ( $unit * $block )
+                        : $values->{min},
+                        ( $block + ( $block * $unit ) - 1 ) > $values->{max}
+                        ? $values->{max}
+                        : $block + ( $block * $unit ) - 1
+                    ]
+                }
+              } $self->search(
+                {
+                    name      => $name,
+                    operation => $operation,
+                    run       => $run,
+                    delay     => {
+                        -between => [
+                            ( $unit * $block ) ? ( $unit * $block )
+                            : $values->{min},
+                            ( $block + ( $block * $unit ) - 1 ) > $values->{max}
+                            ? $values->{max}
+                            : $block + ( $block * $unit ) - 1
+                        ]
+                    },
+                },
+                {
+                    select => [ 'name', 'operation', 'run', { count => '*' } ],
+                    as       => [qw/name operation run total/],
+                    group_by => [qw/name operation run/],
+                }
+              )->all;
+        }
+    }
+    return $stack;
+}
+
 1;
