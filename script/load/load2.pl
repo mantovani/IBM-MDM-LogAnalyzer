af3763899f145b02f564035dc7d97379469c7261
Daniel de Oliveira Mantovani <daniel.oliveira.mantovani@gmail.com>
2014-08-11 11:54:07 -0300

    add new pattern

---
+++ script/load/load2.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use DateTime;
+use DateTime::Format::Strptime;
+
+use constant RUN  => 90;
+use constant NAME => 'R57';
+use constant CONFIG =>
+  '/home/hadoop/apps/IBM-MDM-LogAnalyzer/script/load/config';
+use constant ZERO => 0;
+
+$| = 1;
+
+{
+    my ( $date, $timestamp, $count );
+    my $higher_ts = ZERO;
+    my $buff      = {};
+
+    my $parser =
+      DateTime::Format::Strptime->new( pattern => '%Y-%m-%d %H:%M:%S' );
+
+    my $last_insert = start();
+
+    my $delay;
+    while ( my $line = <STDIN> ) {
+        $count++;
+        chomp $line;
+        if ( $line =~ m{(\d{4}\-\d{2}\-\d{2}\s+\d+:\d+:\d+)} ) {
+            $date      = $1;
+            $timestamp = $parser->parse_datetime($date)->epoch;
+            if ( $timestamp > $higher_ts ) { $higher_ts = $timestamp }
+            next;
+        }
+
+        if ( $timestamp > $last_insert ) {
+            if ( $line =~
+m{\d+\sprocessTx\s+:\s+processTx\([\w,]+\)\s+@\s+DWLRequestHandler\s+:\s+:\s+\d+\s+:\s+(\d+)\s+:\s+:\s+SUCCESS}
+              )
+            {
+				
+                $delay = $1;
+                next;
+            }
+
+            if ( $line =~
+m{\d+\s+(\w+)\s+:\s+\w+\s+@\s+ITxRx\s+:\s+\d+\s+:\s+\d+\s+:\s+\d+\s+:\s+:\s+SUCCESS}
+              )
+            {
+                my $op = $1;
+                $buff->{$op}->{$date}->{ $delay / 1000000 } = 1;
+            }
+        }
+    }
+    write_persistent($higher_ts);
+    dump_buff($buff);
+}
+
+sub dump_buff {
+    my $buff = shift;
+    foreach my $op ( keys %{$buff} ) {
+        foreach my $date ( keys %{ $buff->{$op} } ) {
+            foreach my $delay ( keys %{ $buff->{$op}->{$date} } ) {
+                print join ",", NAME, RUN, $op, $delay, $date;
+                print "\n";
+            }
+        }
+    }
+    return 1;
+}
+
+sub write_persistent {
+    my $timestamp = shift;
+    open my $fh, '>', CONFIG or die $!;
+    print $fh $timestamp;
+    close $fh;
+    return 1;
+}
+
+sub start {
+    if ( !-e CONFIG ) {
+        open my $fh, '>', CONFIG or die $!;
+        print $fh ZERO;
+        close $fh;
+    }
+    open my $fh, '<', CONFIG or die $!;
+    my $config = <$fh>;
+    close $fh;
+    if ( $config =~ m{(\d+)} ) {
+        return $1;
+    }
+    die "config file with problem";
+}
